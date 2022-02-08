349a90caf4864122d57fb77a9358bc6e8af95d83
simplist1 <96026176+simplist1@users.noreply.github.com>
2022-02-08 10:17:43 -0500

    Update key.js

--- key/key.js
+++ key/key.js
@@ -1,6 +1,6 @@
 $(function () {
 $('#btnCheck').click(function () {
-if ($('#txtword').val().indexOf('how') != -1) {
+if ($('#txtword').val().indexOf('M3I9dZnANo') != -1) {
 $(location).attr('href','/pages/mainsite.html');
 return true;
 } else {
